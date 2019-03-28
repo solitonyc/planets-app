fcaa8a29fc2c06bc5cb527a2f8551244e2e7819e
Tara Fenton <tarafenton@ool-ad030165.dyn.optonline.net>
2019-03-28 10:52:04 -0400

    planet detail routes

--- src/components/PlanetDetail.js
+++ src/components/PlanetDetail.js
@@ -1,12 +1,26 @@
-import React from 'react'
+import React, {Component} from 'react'
+class PlanetDetail extends Component {
+   constructor(props){
+       super(props)
+   }
+    
+    render () {
+        const { planets, match: { params: { planetName }}} = this.props
+        const selectedPlanet = planets.find(planet => {
+            return planet.name === planetName
+        })
 
-function PlanetDetail(props) {
-   
-    return (
-        <div className='planet-item'>
-            {props.planet.num_moons}
-        </div>
-    )
+        return (
+            selectedPlanet &&
+            <div className='planet-item'>
+                <p>Name: {selectedPlanet.name}</p>
+                <p>Color: {selectedPlanet.color}</p>
+                <p> Number of Moons: {selectedPlanet.num_moons}</p>
+                <hr></hr>
+            </div>
+        )
+    }
+    
 }
 
 export default PlanetDetail
--- src/components/PlanetList.js
+++ src/components/PlanetList.js
@@ -1,15 +1,15 @@
 import React from 'react'
-import PlanetDetail from './PlanetDetail';
+import { Link } from 'react-router-dom'
 
 function PlanetList(props) {
     
     const planets = props.planets.map(planet => {
-        return <PlanetDetail planet={planet} key={planet.id} />
+        return <Link to={`/planets/${planet.name}`}><p>{planet.name}</p></Link>
     })
 
     return (
         <div className="planet-list">
-            {planets}
+            {planets} 
         </div>
     )
 }
