96fa0b978494e8d9cd4d4c446169c0596d46b683
Tara Fenton <tarafenton@ool-ad030165.dyn.optonline.net>
2019-03-28 10:50:16 -0400

    App with routes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,84 @@
 import React, { Component } from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
 import './App.css';
+import RandomPlanet from './components/RandomPlanet'
+import PlanetList from './components/PlanetList'
+import PlanetDetail from './components/PlanetDetail'
+import Welcome from './components/Welcome'
 
 class App extends Component {
+
+  constructor() {
+    super()
+    this.state = {
+      randomPlanet: '',
+      planets: [],
+    }
+    this.randomClick = this.randomClick.bind(this)
+  }
+  randomAPI() {
+    let api = 'https://wdi-nyc-planets-api.herokuapp.com/planets/random'
+    fetch(api).then(response => response.json())
+      .then(json => {
+        this.setState({ randomPlanet: json.planet })
+      }).catch(e => console.log(e))
+  }
+
+  planetsAPI() {
+    let api = 'https://wdi-nyc-planets-api.herokuapp.com/planets'
+    fetch(api).then(response => response.json())
+      .then(json => {
+        this.setState({ planets: json.planets })
+      }).catch(e => console.log(e))
+  }
+
+  componentDidMount() {
+    this.randomAPI();
+    this.planetsAPI();
+  }
+
+  randomClick() {
+    this.randomAPI()
+  }
+
   render() {
+
     return (
-      <div className="App">
-        test
-      </div>
+      <BrowserRouter>
+        <div className="App">
+          <main>
+
+            <Route exact path="/" render={() => <Welcome />} />
+
+            <Route exact path="/random" render={() => 
+              <RandomPlanet
+                randomPlanet={this.state.randomPlanet}
+                handleClick={this.randomClick} 
+                />} 
+              />
+
+            <Route exact path="/planets" render={() => 
+              <div>
+                
+                <PlanetList
+                  planets={this.state.planets}
+                />
+              </div>
+            } />
+            
+            <Route exact path="/planets/:planetName" render={(props) => 
+              <div>
+                
+                <PlanetDetail
+                  {...props}
+                  planets={this.state.planets}
+                />
+              </div>
+            } />
+          </main>
+        </div>
+
+      </BrowserRouter>
     );
   }
 }
