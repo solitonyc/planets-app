b5deb65bf4720a87c370a550a5e81bc4fd5b91ea
Tara Fenton <tarafenton@ool-ad030165.dyn.optonline.net>
2019-03-28 10:48:58 -0400

    Welcome component

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -1,10 +1,13 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
 
 function Welcome() {
     
     return (
         <div className="welcome">
             Welcome to Planets
+            <Link to="/planets">Planets</Link> | 
+            <Link to="/random">Random</Link>
         </div>
     )
 }
